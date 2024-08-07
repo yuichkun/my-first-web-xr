4f5fd861832ba86e54ce9b0bc8eec152dd1e54d8
yuichkun <yuichkun@gmail.com>
2024-08-08 01:13:01 +0900

    create a basic xr scene

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import 'aframe';
+  import '@ar-js-org/ar.js/aframe/build/aframe-ar.js';
+
+  onMount(() => {
+    // Any additional setup can be done here
+  });
+</script>
+
+<a-scene embedded arjs>
+  <!-- Define a camera with AR.js capabilities -->
+  <a-entity camera></a-entity>
+
+  <!-- Define a 3D object (a simple box in this case) -->
+  <a-box position="0 0.5 0" material="color: red;"></a-box>
+</a-scene>
